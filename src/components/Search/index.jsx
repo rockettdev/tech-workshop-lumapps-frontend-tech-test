e5688e1958ad175312fb0113e61af0f5d9a446ee
Korey <kjr.dev@hotmail.com>
2023-09-19 13:30:34 -0400

    get character info

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -3,9 +3,22 @@ import { useState } from 'react';
 import React from 'react';
 import { TextField, Theme } from '@lumx/react';
 import { mdiMagnify } from '@lumx/icons';
+import axios from 'axios';
 
 function Search() {
   const [searchInput, setSearchInput] = useState('');
+  const url = process.env.REACT_APP_MARVEL_API_KEY;
+
+  const getCharacter = async () => {
+    try {
+      await axios.get(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchInput}&apikey=${url}`).then((res) => {
+        // handle success
+        console.log(res);
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   const handleChange = (e) => {
     setSearchInput(e.target.value);
@@ -13,13 +26,16 @@ function Search() {
   };
 
   return (
-	<input
-		value={searchInput}
-		theme={Theme.dark}
-		placeholder="Search ..."
-		icon={mdiMagnify}
-		onChange={handleChange}
-	/>
+	<form onSubmit={getCharacter}>
+		<input
+			value={searchInput}
+			theme={Theme.dark}
+			placeholder="Search ..."
+			icon={mdiMagnify}
+			onChange={handleChange}
+		/>
+	</form>
+
   );
 }
 

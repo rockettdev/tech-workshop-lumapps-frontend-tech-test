9c9bb59188e93602383c1390ff2682b114b181ed
Korey <kjr.dev@hotmail.com>
2023-09-19 12:47:42 -0400

    search bar changes

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,27 @@
+/* eslint-disable import/no-duplicates */
+import { useState } from 'react';
 import React from 'react';
 import { TextField, Theme } from '@lumx/react';
 import { mdiMagnify } from '@lumx/icons';
 
-const Search = () => (
-	<TextField theme={Theme.dark} placeholder="Search ..." icon={mdiMagnify} onChange={() => {}} />
-);
+function Search() {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+    console.log(searchInput);
+  };
+
+  return (
+	<input
+		value={searchInput}
+		theme={Theme.dark}
+		placeholder="Search ..."
+		icon={mdiMagnify}
+		onChange={handleChange}
+	/>
+  );
+}
+
 
 export default Search;
